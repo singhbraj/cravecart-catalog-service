650e96e0008f062576cbad1b35d0255b2dc86c87
singhbraj <braj333singh@gmail.com>
2024-08-29 00:17:27 +0530

    add databse

--- server.ts
+++ server.ts
@@ -1,13 +1,13 @@
 import app from "./src/app";
 import config from "config";
 import logger from "./src/config/logger";
-// import connectDB from "./src/config/db";
+import connectDB from "./src/config/db";
 
 const startServer = async () => {
   const PORT = config.get("server.port") || 5503;
 
   try {
-    // await connectDB();
+    await connectDB();
 
     app
       .listen(PORT, () => console.log(`Listening on port ${PORT}`))
--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,7 +3,7 @@ import winston from "winston";
 const logger = winston.createLogger({
   level: "info",
   defaultMeta: {
-    serviceName: "order-service",
+    serviceName: "catalog-service",
   },
   format: winston.format.combine(
     winston.format.timestamp(),
