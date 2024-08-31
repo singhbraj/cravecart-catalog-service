6e44ffde1853953f88ff694b49495656d70331e0
singhbraj <braj333singh@gmail.com>
2024-09-01 01:59:25 +0530

    add authentication middleware

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -1,16 +1,22 @@
+import { asyncWrapper } from "./../utils";
 import express from "express";
 import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../config/logger";
-// import authenticate from "../common/middleware/authenticate";
+import authenticate from "../common/middleware/authenticate";
 
 const router = express.Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-router.post("/", categoryValidator, categoryController.create);
+router.post(
+  "/",
+  authenticate,
+  categoryValidator,
+  asyncWrapper(categoryController.create),
+);
 
 // router.get("/", asyncWrapper(categoryController.index));
 // router.get("/:categoryId", asyncWrapper(categoryController.getOne));
